8024b876ccc7c2f6fbf747d0320b4149e8c1ef44
Heath Raftery <heath@empirical.ee>
2026-02-06 21:29:22 +1100

    Re-jigged fire_test_packet function - don't change size on failure
    Realised the spec is expecting the client (eg. management application) to do the testing to find a packet size that works. So all the camera has to do is apply fixed limits, and then attempt the send. If it fails, do nothing!

--- src/Program.cs
+++ src/Program.cs
@@ -18,24 +18,43 @@ namespace GigE_Cam_Simulator
 
             server.OnRegisterChanged(eBootstrapRegister.Stream_Channel_Packet_Size_0, (regMem) =>
             {
-                uint fireTestPacketBitIndex = 0;
-                bool F = regMem.ReadBit(eBootstrapRegister.Stream_Channel_Packet_Size_0, fireTestPacketBitIndex);
-                if (F) //then fire one test packet of size provided in register, and auto-clear
+                //The spec is bit unclear on handling unsupported packet sizes. It says:
+                //  "When a GVSP transmitter cannot support the requested packet_size, then it MUST NOT fire
+                //   a test packet when requested to do so. Also, it MUST round down the packet_size to the
+                //   nearest supported value and update the register."
+                //I now believe we have to *first* decide whether the requested packet size is "supported",
+                //and only then consider the fire test packet bit. If it is not supported, we must "round
+                //down to the nearest supported value". Since we don't actually know what the *network* will
+                //support, we can only act on what *we* support. Alas, as a simulator, we don't have a way
+                //to specify that, unless we add config functionality and get the user to specify.
+                //So for now, use the Min/Max values hard-coded into the Teledyne DALSA Symphony camera.xml:
+                bool isSupportedPacketSize = true; //by default
+                var regVal = regMem.ReadIntBE(eBootstrapRegister.Stream_Channel_Packet_Size_0);
+                var packetSize = regVal & 0x0000FFFF;
+                var roundedPacketSize = Math.Max(512, Math.Min(packetSize, 9216)); //Min/Max for GevSCPSPacketSize
+                if(roundedPacketSize != packetSize)
                 {
-                    if(!server.SendStreamPacket(null))
-                    {
-                        //The packet can fail to send because it's too big to fit in the MTU. The spec says
-                        //"When a GVSP transmitter cannot support the requested packet_size, then it MUST NOT fire
-                        // a test packet when requested to do so. Also, it MUST round down the packet_size to the
-                        // nearest supported value and update the register."
-                        //But I can't see any way to find the "nearest supported value" without sending at test
-                        //least one successful test packet! Next best we can do is the maximum safe UDP payload:
-                        //https://stackoverflow.com/a/35697810/3697870
-                        uint oldVal = regMem.ReadIntBE(eBootstrapRegister.Stream_Channel_Packet_Size_0);
-                        uint newVal = (oldVal & 0xFFFF0000) | 576; //Packet size is in lower two bytes.
-                        regMem.WriteIntBE(eBootstrapRegister.Stream_Channel_Packet_Size_0, (int)newVal);
-                    }
-                    regMem.WriteBit(eBootstrapRegister.Stream_Channel_Packet_Size_0, fireTestPacketBitIndex, false);
+                    isSupportedPacketSize = false;
+                    uint newVal = (regVal & 0xFFFF0000) | roundedPacketSize; //Packet size is in lower two bytes.
+                    //Quietly write it back - the application must then read to see the result
+                    regMem.WriteIntBE(eBootstrapRegister.Stream_Channel_Packet_Size_0, (int)newVal);
+                }
+
+                const uint FIRE_TEST_PACKET_BIT = 0;
+                bool F = regMem.ReadBit(eBootstrapRegister.Stream_Channel_Packet_Size_0, FIRE_TEST_PACKET_BIT);
+                if (F) //then fire one test packet of size provided in register (if valid), and auto-clear
+                {
+                    //Note the send function can return false and the packet can fail to send if it's too big
+                    //to fit in the MTU. But there's I can't see a way to know what will work without sending
+                    //at least one successful test packet! We could round down to the maximum safe UDP
+                    //payload: https://stackoverflow.com/a/35697810/3697870
+                    //But in the spec section 11.5 "Packet Size Negotiation", the expectation seems to be that
+                    //the client tries various sizes until one gets through. So all we have to do is let it
+                    //succeed or quietly fail.
+                    if (isSupportedPacketSize)
+                        server.SendStreamPacket(null);
+                    
+                    regMem.WriteBit(eBootstrapRegister.Stream_Channel_Packet_Size_0, FIRE_TEST_PACKET_BIT, false);
                 }
             });
 
